bc4d316e759ab4788c011c1f80c6a42fc35ff2b7
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-15 01:49:40 +0400

    .

--- lib/hello.ml
+++ lib/hello.ml
@@ -1,7 +1,1 @@
-3110;;
-let x = 42;;
-print_endline "Hello world!";;
-(* #quit;; *)
-let x : int = 3110
-let x : string = 3110
-let increment x = x + 1
+let _ = print_endline "Hello world!"
---
+++ lib/toplevel.ml
@@ -0,0 +1,10 @@
+3110;;
+let x = 42;;
+print_endline "Hello world!";;
+(* #quit;; *)
+let x : int = 3110
+(* let x : string = 3110 *)
+let increment x = x + 1;;
+increment 0
+increment (1)
+increment (increment 5)
