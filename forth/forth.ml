5b6c109391c08fb5223075b79ae3126e90a5e2ff
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-15 12:23:57 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- forth/forth.ml
+++ forth/forth.ml
@@ -19,12 +19,64 @@ let bye () = exit 0
 
 (** {1 VM Memory} *)
 
-(** data stack *)
-let d = ref ([] : cell list)
-
 (** return stack (for nested calls & control structures compilation) *)
 let r = ref ([] : addr list)
 
-(* vocabulary is an ordered list of string:cell pairs *)
+(** data stack *)
+let d : cell Stack.t = Stack.create ()
+
+(** `( -- o )` push item *)
+let push o = Stack.push o d
+
+(** `( o -- )` pop item *)
+let pop () =
+  try Stack.pop d with
+  | Stack.Empty -> failwith "underflow"
+
+(** ( -- ) get top element *)
+let top = Stack.top d
+
+(* ( a -- a a ) *)
+let dup = Stack.dup d
+
+(* ( a -- ) *)
+let drop = Stack.drop d
+
+(* ( a b -- b a ) *)
+let swap = Stack.swap d
+
+(* ( a b -- b ) *)
+let press = Stack.press d
+
+
+(* ( a b -- a b a ) *)
+      let over = Stack.over d
+
+(* (a b c -- b c a) *)
+    let rot = Stack.rot d
+(* (a b c -- c a b ) *)
+    let mrot = Stack.mrot d
+(* ( ... n -- ... d[n] ) fetch n-th item counting from stack top *)
+    let pick = Stack.pick (Stack.pop) d
+
+    (* ( ... -- d.sizeof ) get stack depth *)
+    let depth = Stack.depth d
+
+  (* vocabulary is an ordered list of string:cell pairs *)
 let w =
-  [ ((("nop", nop), ("bye", bye)), ("cr", '\r'), ("lf", '\n'), ("pi", 3.1415)) ]
+  [
+    ( (("nop", nop), ("bye", bye)),
+      ("space", ' '),
+      ("cr", '\r'),
+      ("lf", '\n'),
+      ("pi", 3.1415),
+      ("dup",dup), 
+      ("drop",drop), 
+      ("swap",swap), 
+      ("over",over), 
+      ("rot",rot), 
+      ("-rot",mrot), 
+      ("pick",pick), 
+      ("depth",depth),
+      );
+  ]
